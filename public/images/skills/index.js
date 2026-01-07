7366dd54fa353014c9f7f3c70114a8cdf705bf42
Carlos Coreas <ccoreas484@gmail.com>
2026-01-06 19:14:09 -0500

    Add SVG icons for various skills including AWS, Bootstrap, CSS, HTML, JavaScript, Jest, Material UI, Netlify, React, Redux Saga, SCSS, TypeScript, Express, Node.js, PostgreSQL, and Next.js

---
+++ public/images/skills/index.js
@@ -0,0 +1,35 @@
+import aws from "./aws.svg";
+import bootstrap from "./bootstrap.svg";
+import css from "./css.svg";
+import html from "./html.svg";
+import javascript from "./javascript.svg";
+import jest from "./jest.svg";
+import material_ui from "./material-ui.svg";
+import netlify from "./netlify.svg";
+import react from "./react.svg";
+import redux_saga from "./redux-saga.svg";
+import scss from "./scss.svg";
+import next_js from "./next-js.svg";
+import express_js from "./express-js.svg";
+import node_js from "./node-js.svg";
+import postgresql from "./postgresql.svg";
+import typescript from "./typescript.svg";
+
+export {
+  aws,
+  bootstrap,
+  css,
+  html,
+  javascript,
+  jest,
+  material_ui,
+  netlify,
+  react,
+  redux_saga,
+  scss,
+  next_js,
+  express_js,
+  node_js,
+  postgresql,
+  typescript,
+};
