3ca878767ba0cb5498ace0b8bcdab75f082353be
Carlos Coreas <ccoreas484@gmail.com>
2026-01-07 21:30:01 -0500

    Add SEO optimization skill with corresponding SVG image to skills constant

---
+++ src/constant/skills.tsx
@@ -0,0 +1,58 @@
+export const skills = [
+  {
+    name: "HTML",
+    image: "/images/skills/html.svg",
+  },
+  {
+    name: "CSS",
+    image: "/images/skills/css.svg",
+  },
+  {
+    name: "JavaScript",
+    image: "/images/skills/javascript.svg",
+  },
+  {
+    name: "Seo Techniques",
+    image: "/images/skills/seo-optimization.svg",
+  },
+  {
+    name: "Jest",
+    image: "/images/skills/jest.svg",
+  },
+  {
+    name: "Netlify",
+    image: "/images/skills/netlify.svg",
+  },
+  {
+    name: "React",
+    image: "/images/skills/react.svg",
+  },
+  {
+    name: "Redux Saga",
+    image: "/images/skills/redux-saga.svg",
+  },
+  {
+    name: "SCSS",
+    image: "/images/skills/scss.svg",
+  },
+  {
+    name: "Next.js",
+    image: "/images/skills/next-js.svg",
+  },
+  {
+    name: "Express.js",
+    image: "/images/skills/express-js.svg",
+  },
+  {
+    name: "Node.js",
+    image: "/images/skills/node-js.svg",
+  },
+  {
+    name: "PostgreSQL",
+    image: "/images/skills/postgresql.svg",
+  },
+  {
+    name: "Typescript",
+    image: "/images/skills/typescript.svg",
+  },
+];
