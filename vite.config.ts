71ab3e13789157d9ba43c75ba0e3d88cd93e79f2
Carlos Coreas <ccoreas484@gmail.com>
2026-01-08 18:24:58 -0500

    Remove @tanstack/react-router and related plugins from dependencies and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true,
-    }),
     react(),
   ],
   resolve: {
