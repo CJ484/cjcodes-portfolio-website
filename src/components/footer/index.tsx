28cc838eb4199d888de0a82eae509a661152c05f
Carlos Coreas <ccoreas484@gmail.com>
2026-01-17 23:57:26 -0500

    Add footer message to acknowledge coding tools and inspiration

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,6 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p>© {new Date().getFullYear()} Carlos Coreas. All rights reserved.</p>
+      <p>Coded with React and GSAP and Plenty of Coffee ☕</p>
     </footer>
   )
 }
