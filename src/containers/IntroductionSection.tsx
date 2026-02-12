f31925dd2ecc5108ee9332b67adc01a3761917ad
Carlos Coreas <ccoreas484@gmail.com>
2026-02-12 00:31:43 -0500

    Enhance IntroductionSection with accent text animations

--- src/containers/IntroductionSection.tsx
+++ src/containers/IntroductionSection.tsx
@@ -1,29 +1,106 @@
 import { SplineObject } from "@/components";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/SplitText";
 
 export default function IntroductionSection() {
+  gsap.registerPlugin(SplitText);
+
+  const accentText = [
+    "UX Designer",
+    "Flow",
+    "Animations",
+    "Speed",
+    "Responsive",
+    "Lottie",
+    "Visual Design",
+    "Interactions",
+    "Accessibility",
+    "Fluid",
+    "Design",
+    "Development",
+    "UI/UX",
+    "Web Development",
+    "Frontend Development",
+    "Web Design",
+    "Web Development",
+    "3D Design",
+    "Spline",
+  ];
 
   useGSAP(() => {
     const tl = gsap.timeline();
-    tl.fromTo(".introductionSection__title", {
-      opacity: 0,
-    }, {
-      opacity: 1,
-      duration: 0.5,
-      ease: "power1.inOut",
+    // const elements = gsap.utils.toArray<HTMLElement>(".introductionSection__accentText");
+    // const splits = elements.map((el) => 
+    //   SplitText.create(el, {
+    //     type: "words",
+    //     autoSplit: true,
+    //   })
+    // );
+    // const allWords = splits.flatMap((split) => split.words);
+
+    tl.fromTo(
+      ".introductionSection__title",
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        duration: 0.5,
+        ease: "power1.inOut",
+      },
+    );
+    tl.fromTo(
+      ".introductionSection__subtitle",
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        duration: 0.5,
+        ease: "power1.inOut",
+      },
+    );
+
+    gsap.to(".introductionSection__accentTextContainer__inner-1", {
+      x: "25%",
+      duration: 50,
+      delay: 0.5,
+      repeat: -1,
+      ease: "none",
+      yoyo: true,
     });
-    tl.fromTo(".introductionSection__subtitle", {
-      opacity: 0,
-    }, {
-      opacity: 1,
-      duration: 0.5,
-      ease: "power1.inOut",
+
+    gsap.to(".introductionSection__accentTextContainer__inner-2", {
+      x: "-25%",
+      duration: 50,
+      delay: 0.5,
+      repeat: -1,
+      ease: "none",
+      yoyo: true,
     });
   });
-  
+
   return (
     <section id="introduction" className="introductionSection">
+      <div className="introductionSection__accentTextContainer">
+        <div className="introductionSection__accentTextContainer__inner-1">
+          {accentText
+            .slice(accentText.length / 2, accentText.length)
+            .map((text, index) => (
+              <span key={index} className="introductionSection__accentText">
+                {text}
+              </span>
+            ))}
+        </div>
+        <div className="introductionSection__accentTextContainer__inner-2">
+          {accentText.slice(0, accentText.length / 2).map((text, index) => (
+            <span key={index} className="introductionSection__accentText">
+              {text}
+            </span>
+          ))}
+        </div>
+      </div>
       <div className="container">
         <div className="introductionSection__content">
           <h1 className="introductionSection__title">
